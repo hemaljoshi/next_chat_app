d0dfc7533b0440937ea130515c313ad37dff0f62
joshihemal <hemaljoshi738@gmail.com>
2023-11-19 19:59:17 +0530

    feat- added avatar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -3,14 +3,22 @@ import React from 'react'
 
 import useRoutes from '@/app/hooks/useRoutes';
 import DesktopItem from './DesktopItem';
+import { User } from '@prisma/client';
+import Avatar from '../Avatar';
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+    currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
     const routes = useRoutes()
     const [isOpen, setIsOpen] = React.useState(false)
 
     const toggleSidebar = () => {
         setIsOpen(!isOpen)
     }
+
+    console.log(currentUser)
 
     return (
         <div className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between'>
@@ -27,6 +35,11 @@ const DesktopSidebar = () => {
                         />
                     ))}
                 </ul>
+            </nav>
+            <nav className='mt-4 flex flex-col justify-between items-center'>
+                <div className='cursor-pointer hover:opacity-75 transition' onClick={() => setIsOpen(true)}>
+                    <Avatar user={currentUser} />
+                </div>
             </nav>
         </div>
     )
